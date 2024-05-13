554fe0a897cfd8a0d62b2dfdea1b75c2e5f3b211
Aleix Roca Nonell <aleix.rocanonell@bsc.es>
2024-05-13 19:55:25 +0200

    Convert weaponStore into a static class

--- library/public/frontend/characterSheet.ts
+++ library/public/frontend/characterSheet.ts
@@ -1,26 +1,33 @@
 async function myFunction() {
-  console.log("MyFunc function called!");
-  console.log("panel name: " +MapTool.getName());
-  // TODO consider encoding data https://discord.com/channels/296230822262865920/1228749752565497916/1228762433330348042
-  // another way of handling this from a non-async function: MapTool.getUserData().then(obj => console.log(JSON.stringify(obj)));
-  const dataStr: string = await MapTool.getUserData();
-  const data: any = JSON.parse(dataStr);
-  const tokenId = data["currentTokenId"]
-  console.log("getUserData: " + dataStr);
-  console.log(" - currentTokenId: " + tokenId);
-  console.log("=====");
-  const rttStr: any = await callMTScriptMacro('[r: js.getRobotta("'+tokenId+'")]');
-  console.log("value returned: " + rttStr);
-  const rttObj: RobottaData = JSON.parse(rttStr);
-  const lazuly: any = new Robotta(rttObj);
-  console.log("object created: " + JSON.stringify(lazuly));
+	try {
+		console.log("MyFunc function called!");
+		console.log("panel name: " +MapTool.getName());
+		// TODO consider encoding data https://discord.com/channels/296230822262865920/1228749752565497916/1228762433330348042
+		// another way of handling this from a non-async function: MapTool.getUserData().then(obj => console.log(JSON.stringify(obj)));
+		const dataStr: string = await MapTool.getUserData();
+		const data: any = JSON.parse(dataStr);
+		const tokenId = data["currentTokenId"]
+		console.log("getUserData: " + dataStr);
+		console.log(" - currentTokenId: " + tokenId);
+		console.log("=====");
+		const rttStr: any = await callMTScriptMacro('[r: js.getRobotta("'+tokenId+'")]');
+		console.log("value returned: " + rttStr);
+		const rttObj: RobottaData = JSON.parse(rttStr);
+		const lazuly: any = new Robotta(rttObj);
+		console.log("object created: " + JSON.stringify(lazuly));
 
-
-
-
-  // this does not work! The whole script refuses to run without a single
-  // error. I think that I should use a try catch block
-  //console.log("token: " + JSON.stringify(token));
+		for (let weaponSlot of lazuly.weapons) {
+			const id = weaponSlot.id;
+			const weapon = WeaponStore.getItem(id);
+			console.log(" - "+JSON.stringify(weapon));
+		}
+		console.log("done!");
+	}catch (error: any) {
+		console.log("### callMTScriptMacro error: " + error.stack);
+	}
+	// this does not work! The whole script refuses to run without a single
+	// error. I think that I should use a try catch block
+	//console.log("token: " + JSON.stringify(token));
 }
 
 async function callMTScriptMacro(macro: string) {
