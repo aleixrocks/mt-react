e32fbe0d25afa85860ae6ffb14c373efb831cb7f
Aleix Roca Nonell <aleix.rocanonell@bsc.es>
2024-05-26 13:17:50 +0200

    Import ts file and use it in react

--- rtt/src/App.tsx
+++ rtt/src/App.tsx
@@ -1,24 +1,46 @@
+//import React from 'react';
+//import logo from './logo.svg';
+//import './App.css';
+//
+//function App() {
+//  return (
+//    <div className="App">
+//      <header className="App-header">
+//        <img src={logo} className="App-logo" alt="logo" />
+//        <p>
+//          Edit <code>src/App.tsx</code> and save to reload.
+//        </p>
+//        <a
+//          className="App-link"
+//          href="https://reactjs.org"
+//          target="_blank"
+//          rel="noopener noreferrer"
+//        >
+//          Learn React
+//        </a>
+//      </header>
+//    </div>
+//  );
+//}
+//
+//export default App;
+
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import {WeaponStore} from './WeaponStore';
 
 function App() {
+  const [count, setCount] = React.useState(0);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <img src={logo} alt="logo" />
+      <h1>Hello TypeScript!</h1>
+      <p>{WeaponStore.items[0].name}</p>
+      <p>You clicked {count} times</p>
+      <button onClick={() => setCount(count + 1)}>
+        Click me
+      </button>
     </div>
   );
 }
