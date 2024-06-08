89922057cdb21b17d4ea8057a76a71bb6b428b2a
Aleix Roca Nonell <aleix.rocanonell@bsc.es>
2024-06-08 13:30:53 +0200

    Fix MapTool fallback in backend

--- tmp/backend/src/RobottaUtils.ts
+++ tmp/backend/src/RobottaUtils.ts
@@ -1,49 +1,55 @@
-try {
-	if (MapTool && typeof MapTool === "object") {
-		MapTool.chat.broadcast("Running in Maptool");
-	} else {
-		console.log("Running in Browser");
-		throw new Error("browser");
-	}
-} catch (e: any) {
-	// we are running in a browser where maptool is not defined. Define now a placeholder.
-	class Token {
-		id: string
-		properties: any;
+class Token {
+	id: string
+	properties: any;
 
-		constructor(id:string) {
-			this.properties = {};
-			this.id = id;
-		}
-
-		getProperty(property: string): string {
-			return this.properties[property];
-		}
-
-		setProperty(property: string, value: string) {
-			this.properties[property] = value;
-		}
+	constructor(id:string) {
+		this.properties = {};
+		this.id = id;
 	}
 
-	class Tokens {
-		static tokenList:any = {};
-
-		static getTokenByID(tokenId:string): Token {
-			return Tokens.tokenList[tokenId] ?? (Tokens.tokenList[tokenId] = Token(tokenId));
-		}
+	getProperty(property: string): string {
+		return this.properties[property];
 	}
 
-	class Chat {
-		static broadcast(msg:string) {
-			console.log(msg);
-		}
-	}
-
-	class MapTool {
-		static tokens = Tokens;
-		static chat = Chat;
+	setProperty(property: string, value: string) {
+		this.properties[property] = value;
 	}
 }
+
+class Tokens {
+	static tokenList:any = {};
+
+	static getTokenByID(tokenId:string): Token {
+		return Tokens.tokenList[tokenId] ?? (Tokens.tokenList[tokenId] = Token(tokenId));
+	}
+}
+
+class Chat {
+	static broadcast(msg:string) {
+		console.log(msg);
+	}
+}
+
+class FallbackMapTool {
+	static tokens = Tokens;
+	static chat = Chat;
+}
+
+class FallbackMTScript {
+	static registerMacro(name: string, func: any) {
+		// TODO expose API
+	}
+}
+
+if (typeof globalThis.MapTool !== "object") {
+	console.log("Running in a server!");
+	globalThis.MapTool = FallbackMapTool;
+	globalThis.MTScript = FallbackMTScript;
+} else {
+	MapTool.chat.broadcast("Running in MapTool!");
+}
+
+MapTool.chat.broadcast("MapTool object ready!");
 
 export const RobottaUtils = {
 	getToken(tokenId: string): Token {
