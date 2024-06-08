2eaa221f6507b29b3f6cc82a2271e3b137a1a51c
Aleix Roca Nonell <aleix.rocanonell@bsc.es>
2024-06-08 17:28:52 +0200

    Restore react backend-frotend communication in maptool

--- tmp/frontend/src/App.tsx
+++ tmp/frontend/src/App.tsx
@@ -25,24 +25,40 @@
 //
 //export default App;
 
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
+import {FrontendUtils} from './FrontendUtils';
+import {Robotta} from 'shared/dist/Robotta';
 import {WeaponStore} from 'shared/dist/WeaponStore';
 
-function App() {
-  const [count, setCount] = React.useState(0);
+let initialRtt: Robotta | null;
+let tokenId: string;
+(async () => {
+})();
 
-  return (
-    <div>
-      <img src={logo} alt="logo" />
-      <h1>Hello TypeScript!</h1>
-      <p>{WeaponStore.items[0].name}</p>
-      <p>You clicked {count} times</p>
-      <button onClick={() => setCount(count + 1)}>
-        Click me
-      </button>
-    </div>
-  );
+function App() {
+	const [count, setCount] = useState(0);
+	const [rtt, setRtt] = useState<Robotta|null>(null);
+	const [id, setId] = useState("unknown id");
+
+	useEffect(() => {
+		FrontendUtils.getCurrentRobotta().then(nrtt => setRtt(nrtt));
+		FrontendUtils.getCurrentTokenId().then(id => setId(id));
+	}, []);
+	
+	console.log("APP: tokenid: " + id);
+	console.log("APP: initalRTT: " + JSON.stringify(rtt));
+
+	return (
+		<div>
+			<h1>Name: {rtt?.name}</h1>
+			<h2>Token ID: {id}</h2>
+			<p>{WeaponStore.items[0].name}</p>
+			<p>You clicked {count} times</p>
+			<button onClick={() => setCount(count + 1)}>
+				Click me
+			</button>
+		</div>
+	);
 }
 
 export default App;
---
+++ tmp/frontend/src/FrontendUtils.tsx
@@ -0,0 +1,57 @@
+import {Robotta, RobottaData} from 'shared/dist/Robotta';
+
+
+class FallbackMapTool {
+	static async getUserData(): Promise<any> {
+		return new Promise ((resolve, reject) => {
+			resolve({currentTokenId: "fakeTokenId"});
+		});
+	}
+}
+
+if (typeof (globalThis as any).MapTool !== "object") {
+	console.log("Running in a Browser!");
+	(globalThis as any).MapTool = FallbackMapTool;
+} else {
+	console.log("Running in MapTool!");
+}
+
+export class FrontendUtils {
+	static async getCurrentRobotta(): Promise<Robotta | null> {
+		const tokenId: string = await FrontendUtils.getCurrentTokenId();
+		const rtt: Robotta | null = await FrontendUtils.getRobotta(tokenId);
+		return rtt;
+	}
+
+	static async getCurrentTokenId(): Promise<string> {
+		const dataStr: string = await MapTool.getUserData();
+		const data: any = JSON.parse(dataStr);
+		const tokenId = data["currentTokenId"];
+		return tokenId;
+	}
+
+	static async getRobotta(tokenId: string): Promise<Robotta | null> {
+		const rttStr: any = await FrontendUtils.callMTScriptMacro('[r: js.getRobotta("'+tokenId+'")]');
+		const rttObj: RobottaData = JSON.parse(rttStr);
+		const rtt: Robotta = new Robotta(rttObj);
+		if (!rtt) {
+			console.log(`Error: Could not load Robotta with id ${tokenId}`);
+			return null;
+		}
+		return rtt;
+	}
+
+	static async callMTScriptMacro(macro: string): Promise<any> {
+		try {
+			console.log("### calling callMTScriptMacro!");
+			let uri = "macro:evaluateMacro@lib:com.gitlab.aleixrocks.robotta";
+			let r = await fetch(uri, { method: "POST", body: macro });
+			let result = await r.text();
+			console.log("### callMTScriptMacro result="+result);
+			return result;
+		} catch (error: any) {
+			console.log("### callMTScriptMacro error: " + error.stack);
+		}
+		return null;
+	}
+}
